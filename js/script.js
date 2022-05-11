024dca77502c9ed2b244ae7b8df802581389da7d
davide1217 <davidepizzola17@gmail.com>
2022-05-11 16:39:39 +0200

    completo+bonus

--- js/script.js
+++ js/script.js
@@ -13,20 +13,27 @@ const app = new Vue({
   mounted() {
 
     this.emailGenerator();
-    this.loaded = true;
+  
   },
 
   methods: {
     
     emailGenerator() {
+      
       for(let i = 0; i < 10; i++) {
         axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
         .then((resp) => {
 
           this.emails.push(resp.data.response)
-
-        })
+          
+        });
       }
+
+      setTimeout(() => {
+        
+        this.loaded = true;
+        
+      }, 3000)
 
     },
 
